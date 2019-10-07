4bc936869dc05085b657627d716397eb8be37e66
unknown <ykcai@uwaterloo.ca>
2019-10-07 19:55:58 -0400

    added week5 hmwk template

---
+++ homework/week4_homework_answers.py
@@ -0,0 +1,45 @@
+# Machine Learning Class Week 4 Homework
+
+'''
+0. Create a list containing five different sandwich ingredients, such as the following:
+>>> ingredients = ['snails', 'leeches', 'gorilla belly-button lint', 'caterpillar eyebrows', 'centipede toes’]
+Now create a loop that prints out the list (including the numbers):
+>>> ingredients = ["ham","lettuce","tomatos","sauce","green pepper"]
+Create the lists using the .append() method
+'''
+# --------------------------------Code between the lines!--------------------------------
+
+
+# ---------------------------------------------------------------------------------------
+
+
+'''
+1. Your Weight on the Moon:
+If you were standing on the moon right now, your weight would be
+16.5 percent of what it is on Earth. You can calculate that by multiplying
+your Earth weight by 0.165.
+If you gained a kilo in weight every year for the next 15 years,
+what would your weight be when you visited the moon each year
+and at the end of the 15 years? 
+Write a program using a for loop that prints your moon weight for each year.
+'''
+# --------------------------------Code between the lines!--------------------------------
+
+
+# ---------------------------------------------------------------------------------------
+
+
+'''
+2. 
+Instead of using math multiply op, please make use of range to do the multiplication. 
+Hint: rang(start, end, step)  step is the increment to add. By default, it is 1.
+Examples:
+Input : n = 2, m = 3
+Output :  6 
+Input : n = 3, m = 4
+Output :  12
+'''
+# --------------------------------Code between the lines!--------------------------------
+
+
+# ---------------------------------------------------------------------------------------
