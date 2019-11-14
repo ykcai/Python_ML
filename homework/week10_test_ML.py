a895ded5221b96e545c488dd7538381f056fb2ee
Michael Cai <ykcai@uwaterloo.ca>
2019-11-13 22:44:01 -0500

    changed week 10 test

---
+++ homework/week10_test_ML.py
@@ -0,0 +1,53 @@
+# Question 1
+# What is the output of the following program :
+print("Hello World"[::-1])
+# 1. dlroW olleH
+# 2. Hello Worl
+# 3. d
+# 4. Error
+
+# Question 2
+# Write a NumPy program to create an array of 10 zeros
+
+
+# Question 3
+# Write a NumPy program to create an array of 10 ones
+
+
+# Question 4
+# Write a NumPy program to create an array of the integers from 10 to 50
+
+
+# Question 5
+# What is the output of the following program:
+y = 8
+def z(x): return x * y
+print(z(6))
+# 48
+# 14
+# 64
+# 4. None of the above
+
+# Question 6
+# What is the output of the following code :
+L = ['a', 'b', 'c', 'd']
+print("".join(L))
+# 1. Error
+# 2. None
+# 3. abcd
+# 4. [‘a’,’b’,’c’,’d’]
+
+# Question 7
+# Suppose you have a guest list of students and the country they are from, stored as key-value pairs in a dictionary.
+GUEST_LIST = {
+"Randy": "Germany",
+"Karla": "France",
+"Wendy": "Japan",
+"Norman": "England",
+"Sam": "Argentina"
+}
+# Write a function that takes in a name and returns a name tag, that should read:
+# "Hi! I'm [name], and I'm from [country]."
+# If the name is not in the dictionary, return:
+# "Hi! I'm a guest."
+# Hint: In order to grab a value from a dictionary you can use indexing like GUEST_LIST["Randy"]
