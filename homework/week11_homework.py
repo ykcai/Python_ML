39ae562c11bfb292d66caa24d8285c5c02e9ca2c
Michael Cai <ykcai@uwaterloo.ca>
2019-11-24 18:37:06 -0500

    Added week 11 homework

---
+++ homework/week11_homework.py
@@ -0,0 +1,20 @@
+# Machine Learning Class Week 11 Homework
+# 1. An isogram is a word that has no repeating letters, consecutive or nonconsecutive. Create a function that takes a string and returns either True or False depending on whether or not it's an "isogram".
+
+# Examples
+# is_isogram("Algorism") ➞ True
+# is_isogram("PasSword") ➞ False
+# # Not case sensitive.
+# is_isogram("Consecutive") ➞ False
+# Notes: Ignore letter case (should not be case sensitive). All test cases contain valid one word strings.
+
+
+def is_isogram(word):
+    pass
+
+
+is_isogram("Algorism")
+is_isogram("PasSword")
+is_isogram("Consecutive")
+
+# 2. What are the 4 different detection models that we covered in class?
